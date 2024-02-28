074a1c0f6be45c2df63f1a1f3241431314142206
Md Kamran <mdkamran7255@gmail.com>
2024-02-28 13:28:34 +0530

    Add 404 page to client side

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,17 @@
-import Link from 'next/link'
+"use client"
+import { useRouter } from 'next/navigation'
+
  
 export default function NotFound() {
+  const router = useRouter()
+  const handleRoute = ():void =>{
+    router.push("/")
+  }
   return (
     <div className="w-[100vw] h-[100vh] flex flex-col gap-1 justify-center items-center">
       <h2>Not Found</h2>
       <p>Could not find requested page</p>
-      <Link href="/" className='rounded-lg hover:cursor-pointer p-3 bg-purple-700 hover:bg-purple-600'>Return To Dashboard</Link>
+      <button onClick={handleRoute} className='rounded-lg hover:cursor-pointer p-3 bg-purple-700 hover:bg-purple-600'>Return To Dashboard</button>
     </div>
   )
 }
